1b5fa60f4b261f610effaa5bab71f088f984ab71
Adam Oneil <adamsoftware@gmail.com>
2022-05-29 08:56:23 -0400

    #3 DataSet support

--- DataTables.Library/SqlConnectionExtensions.cs
+++ DataTables.Library/SqlConnectionExtensions.cs
@@ -17,6 +17,12 @@ namespace DataTables.Library
         public static async Task<DataTable> QueryTableAsync(this SqlConnection connection, string sql, object parameters = null, CommandType? commandType = null) => 
             await new SqlServerQueryRunner().QueryTableAsync(connection, sql, parameters, commandType);
 
+        public static DataSet QueryDataSet(this SqlConnection connection, string sql, object paramters = null, CommandType? commandType = null) =>
+            new SqlServerQueryRunner().QueryDataSet(connection, sql, paramters, commandType);
+
+        public static async Task<DataSet> QueryDataSetAsync(this SqlConnection connection, string sql, object paramters = null, CommandType? commandType = null) =>
+            await new SqlServerQueryRunner().QueryDataSetAsync(connection, sql, paramters, commandType);
+
         public static DataTable QuerySchemaTable(this SqlConnection connection, string sql, object parameters = null) =>
             new SqlServerQueryRunner().QuerySchemaTable(connection, sql, parameters);
 
